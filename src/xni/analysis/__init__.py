d9df21be70bd773f8c9940ddec324a100fb5691f
vdsluser <thinkout@gmail.com>
2026-08-25 13:44:27 +0900

    feat: export network analysis summary

--- src/xni/analysis/__init__.py
+++ src/xni/analysis/__init__.py
@@ -1,11 +1,14 @@
 from .centrality import CentralNodeScore, rank_central_nodes
 from .network import FollowingSimilarity, find_new_account_cohort_pairs, find_similarity_pairs
 from .profiles import AccountProfileSignal, find_new_account_candidates
+from .summary import NetworkAnalysisSummary, build_network_summary
 
 __all__ = [
     "AccountProfileSignal",
     "CentralNodeScore",
     "FollowingSimilarity",
+    "NetworkAnalysisSummary",
+    "build_network_summary",
     "find_new_account_candidates",
     "find_new_account_cohort_pairs",
     "find_similarity_pairs",
