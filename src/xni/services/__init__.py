2ad0a6d8bb3992325de65c6dfb57064a076442f9
vdsluser <thinkout@gmail.com>
2026-08-25 13:44:35 +0900

    feat: export expansion services

--- src/xni/services/__init__.py
+++ src/xni/services/__init__.py
@@ -1,3 +1,22 @@
+from .batch import BatchImportRequest, BatchImportResult, import_manual_batch
+from .expansion import (
+    ExpansionQueueItem,
+    ExpansionQueueRefresh,
+    list_expansion_queue,
+    promote_expansion_candidate,
+    refresh_expansion_queue,
+)
 from .snapshots import ImportSummary, import_manual_snapshot
 
-__all__ = ["ImportSummary", "import_manual_snapshot"]
+__all__ = [
+    "BatchImportRequest",
+    "BatchImportResult",
+    "ExpansionQueueItem",
+    "ExpansionQueueRefresh",
+    "ImportSummary",
+    "import_manual_batch",
+    "import_manual_snapshot",
+    "list_expansion_queue",
+    "promote_expansion_candidate",
+    "refresh_expansion_queue",
+]
